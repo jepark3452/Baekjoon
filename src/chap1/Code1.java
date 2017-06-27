6aec3af545db77f56f63eed60d4994c15beeb81c
DESKTOP-IN5RT44\jepark <jepark3452@gmail.com>
2017-06-27 17:48:56 +0900

    * create project - 1챕터 commit

---
+++ src/chap1/Code1.java
@@ -0,0 +1,20 @@
+package chap1;
+
+/**
+ * 2557번 
+ * Hello World!를 출력하시오.
+ * 
+ * 입력 없음
+ * 
+ * 출력 Hello World!를 출력하시오.
+ * 
+ * @author jepark
+ *
+ */
+public class Code1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
