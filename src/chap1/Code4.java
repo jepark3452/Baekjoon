f2ea894894c6abcb08d5744d5f211d6fff3af68f
DESKTOP-IN5RT44\jepark <jepark3452@gmail.com>
2017-06-27 18:41:46 +0900

    * Chap1-4 commit

---
+++ src/chap1/Code4.java
@@ -0,0 +1,21 @@
+package chap1;
+
+/**
+ * 
+ * 7287번
+ * 
+ * 입력 이 문제는 입력이 없다.
+ * 
+ * 출력 첫 줄에 자신이 맞은 문제의 수, 둘째 줄에 아이디를 출력한다.
+ * 
+ * @author jepark
+ *
+ */
+public class Code4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(3);
+		System.out.println("jepark3452");
+	}
+}
