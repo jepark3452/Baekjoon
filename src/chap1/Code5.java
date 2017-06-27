e56f2528e02d4255ceddd409bf3c38a35e47d6d1
DESKTOP-IN5RT44\jepark <jepark3452@gmail.com>
2017-06-27 18:51:01 +0900

    * Chap1-5 commit

---
+++ src/chap1/Code5.java
@@ -0,0 +1,26 @@
+package chap1;
+
+/**
+ * 
+ * 
+ * 10172번
+ * 문제 아래 예제와 같이 개를 출력하시오.
+ * 
+ * 입력 없음.
+ * 
+ * 출력 개를 출력한다.
+ * 
+ * @author jepark
+ *
+ */
+public class Code5 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\ ");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
